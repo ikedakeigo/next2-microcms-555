071d40e96af1a7b9eceb4b02952d448850e3abcc
kento <kento@toriwatari.bird>
2024-09-01 19:50:35 +0900

    リセットCSSの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import styles from './page.module.css'
+// import styles from './page.module.css'
 import { client } from '../../libs/client'
 
 export default async function Home() {
@@ -7,9 +7,15 @@ export default async function Home() {
   })
 
   return (
+    <main>
+      <h1>h1: サイト名</h1>
+      <h2>h2: サイト名</h2>
+      <p>{data.title}</p>
+    </main>
+    /*
     <main className={styles.main}>
-
       <div className={styles.center}>{data.title}</div>
     </main>
+    */
   )
 }
